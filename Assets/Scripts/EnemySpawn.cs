219ba5245a60526ef60cc069b306a8839e7ee69f
thomassontyler <59318453+thomassontyler@users.noreply.github.com>
2022-08-15 16:09:03 -0400

    #Desktop

--- Assets/Scripts/EnemySpawn.cs
+++ Assets/Scripts/EnemySpawn.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class EnemySpawn : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        Destroy(gameObject, 2.5f);
+        
     }
 
-    void OnCollisionEnter()
+    // Update is called once per frame
+    void Update()
     {
-        Destroy(gameObject);
+        
     }
 }
